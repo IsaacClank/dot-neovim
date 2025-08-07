c11137d177b0341b2cca700d951a75941f07d2d1
isaac <minimaleffort769@gmail.com>
2025-08-07 03:48:00 +0000

    fix: conflicted mapping
    Mapping conflicts caused by mini.surround and yank. Fixed by using the
    default key maps for mini.surround.

--- lua/plugins/essentials.lua
+++ lua/plugins/essentials.lua
@@ -29,17 +29,6 @@ return {
     "echasnovski/mini.surround",
     branch = "main",
     opts = {
-      mappings = {
-        add = "ys",
-        delete = "ds",
-        find = "",
-        find_left = "",
-        highlight = "",
-        replace = "cs",
-        update_n_lines = "",
-        suffix_last = "",
-        suffix_next = "",
-      },
       search_method = "cover_or_next",
     }
   },
