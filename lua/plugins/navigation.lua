e973bbadb4b2feeb489fab88f53165f47f8401cc
Isaac <minimaleffort768@gmail.com>
2025-09-15 15:10:35 +0700

    feat: updated Telescope config

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -6,7 +6,11 @@ return {
     dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", },
     opts = {
       defaults = {
-        layout_strategy = 'vertical'
+        sorting_strategy = 'ascending',
+        layout_strategy = 'flex',
+        layout_config = {
+          prompt_position = "top"
+        }
       },
     },
     keys = {
