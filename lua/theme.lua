047f850142f9dad9b561fe1519a8d0413df14bd5
Isaac <minimaleffort768@gmail.com>
2025-09-17 13:00:10 +0700

    feat: vscode colorscheme

--- lua/theme.lua
+++ lua/theme.lua
@@ -2,6 +2,10 @@ local deps = require('mini.deps')
 local M = {}
 
 M.setup = function()
+  deps.add('mofiqul/vscode.nvim')
+  deps.add('rrethy/base16-nvim')
+  deps.add('vague2k/vague.nvim')
+
   deps.now(function()
     require('mini.icons').setup({})
     require('mini.icons').mock_nvim_web_devicons()
@@ -11,7 +15,7 @@ M.setup = function()
       }
     })
 
-    vim.cmd [[colorscheme chalk]]
+    vim.cmd [[colorscheme vscode]]
     -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'None' })
     -- vim.api.nvim_set_hl(0, 'PmenuSel', { bg = 'NvimDarkGrey4', fg = 'None', reverse = false })
     -- vim.api.nvim_set_hl(0, 'PmenuMatch', { underline = true })
