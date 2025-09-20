0d72f5e148572a51e10544030fd346b130513b25
Isaac <minimaleffort769@gmail.com>
2025-09-21 00:53:52 +0700

    refactor: options

--- lua/options.lua
+++ lua/options.lua
@@ -1,13 +1,13 @@
 local M = {}
 
 M.setup = function()
-  vim.o.scrolloff = 10        -- Minimum lines offset on top and bottom. Used to add padding.
+  vim.o.scrolloff = 10       -- Minimum lines offset on top and bottom. Used to add padding.
 
-  vim.o.expandtab = true      -- In insert mode, expand tabs into spaces.
-  vim.o.shiftwidth = 2        -- Number of spaces used for auto-indent.
-  vim.o.tabstop = 2           -- Number of spaces to render tabs in a file. This does **not** modify the file.
+  vim.o.expandtab = true     -- In insert mode, expand tabs into spaces.
+  vim.o.shiftwidth = 2       -- Number of spaces used for auto-indent.
+  vim.o.tabstop = 2          -- Number of spaces to render tabs in a file. This does **not** modify the file.
 
-  vim.o.termguicolors = true; -- Enable 24-bit RGB colors
+  vim.o.termguicolors = true -- Enable 24-bit RGB colors
 
   vim.g.loaded_node_provider = 0;
   vim.g.loaded_perl_provider = 0;
