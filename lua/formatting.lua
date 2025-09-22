1b99f790d7462b2c648ec023f0301c70fddaa779
Isaac <minimaleffort768@gmail.com>
2025-09-22 10:40:43 +0700

    feat(formatting): re-enable format-on-save

--- lua/formatting.lua
+++ lua/formatting.lua
@@ -25,10 +25,10 @@ M.setup = function()
       default_format_opts = {
         lsp_format = "fallback",
       },
-      -- format_on_save = {
-      --   lsp_format = "fallback",
-      --   timeout_ms = 500,
-      -- },
+      format_on_save = {
+        lsp_format = "fallback",
+        timeout_ms = 500,
+      },
       formatters = {
         prisma = {
           command = "npx",
