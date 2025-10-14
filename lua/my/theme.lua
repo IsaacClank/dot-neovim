6974e6f350733d6112eff5ffd0a00c81e0209187
Isaac <minimaleffort768@gmail.com>
2025-10-14 17:29:10 +0700

    feat(theme): randomized theme on startup

--- lua/my/theme.lua
+++ lua/my/theme.lua
@@ -19,9 +19,42 @@ M.setup = function()
     }
   })
 
-  require('catppuccin').setup({
-    transparent_background = true
-  })
-  vim.cmd [[colorscheme catppuccin]]
+  deps.now(function()
+    local themes = {
+      { name = "catppuccin", },
+      { name = "catppuccin-frappe", },
+      { name = "catppuccin-macchiato", },
+      { name = "catppuccin-mocha", },
+      { name = "everforest" },
+      { name = "kanagawa" },
+      { name = "kanagawa-dragon" },
+      { name = "kanagawa-wave" },
+      { name = "monokai" },
+      { name = "monokai_pro" },
+      { name = "monokai_ristretto" },
+      { name = "monokai_soda" },
+      { name = "moonfly" },
+      { name = "onedark" },
+      { name = "onedark_dark" },
+      { name = "onedark_vivid" },
+      { name = "rose-pine-main" },
+      { name = "rose-pine-moon" },
+      { name = "tokyonight" },
+      { name = "tokyonight-moon" },
+      { name = "tokyonight-night" },
+      { name = "tokyonight-storm" },
+      { name = "vague" },
+      { name = "vscode" },
+    }
+
+    math.randomseed(os.time())
+    local random_theme = vim.tbl_get(themes, math.random(1, vim.tbl_count(themes)))
+    local random_theme_name = vim.tbl_get(random_theme, "name")
+    local random_theme_setup = vim.tbl_get(random_theme, "setup")
+    if random_theme_setup ~= nil then
+      random_theme_setup()
+    end
+    vim.cmd('colorscheme ' .. random_theme_name)
+  end)
 end
 return M
