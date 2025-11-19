ca0b20069e9a135c3fea3bdb1cdb03aeaf10ca49
Isaac <minimaleffort769@gmail.com>
2025-11-19 17:20:10 +0700

    feat(session): restore session on VimEnter

--- lua/my/session.lua
+++ lua/my/session.lua
@@ -12,11 +12,25 @@ M.setup = function()
 
 	vim.api.nvim_create_user_command("SessionRestore", function()
 		session.read()
-	end, { desc = "Save session" })
+	end, { desc = "Restore session" })
 
 	vim.api.nvim_create_user_command("SessionSelect", function()
 		session.select()
-	end, { desc = "Save session" })
+	end, { desc = "Select session" })
+
+	vim.api.nvim_create_autocmd("VimEnter", {
+		callback = function()
+			local started_with_file_argument = vim.iter(vim.v.argv):any(function(arg)
+				local stat = vim.uv.fs_stat(arg)
+				return stat ~= nil and stat.type == "file"
+			end)
+
+			if not started_with_file_argument then
+				session.read()
+			end
+		end,
+		nested = true,
+	})
 end
 
 return M
