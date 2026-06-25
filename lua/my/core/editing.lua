b008b97fbbdcaa59d17a8cc1a2680f39e789589a
Isaac <minimaleffort769@gmail.com>
2026-06-25 14:16:50 +0700

    feature(picker|fzf,editing): synchronous setup

--- lua/my/core/editing.lua
+++ lua/my/core/editing.lua
@@ -11,24 +11,22 @@ function mod.setup()
 		"https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
 	})
 
-	vim.schedule(function()
-		require("leap").setup({})
-		vim.keymap.set("n", "gl", "<Plug>(leap)", { desc = "Leap" })
+	require("leap").setup({})
+	vim.keymap.set("n", "gl", "<Plug>(leap)", { desc = "Leap" })
 
-		require("mini.pairs").setup({})
-		require("mini.surround").setup({})
-		require("mini.splitjoin").setup({})
-		require("nvim-treesitter-textobjects").setup({})
+	require("mini.pairs").setup({})
+	require("mini.surround").setup({})
+	require("mini.splitjoin").setup({})
+	require("nvim-treesitter-textobjects").setup({})
 
-		local select_textobject =
-			require("nvim-treesitter-textobjects.select").select_textobject
+	local select_textobject =
+		require("nvim-treesitter-textobjects.select").select_textobject
 
-		vim.keymap.set({ "x", "o" }, "af", function()
-			select_textobject("@function.outer", "textobjects")
-		end)
-		vim.keymap.set({ "x", "o" }, "if", function()
-			select_textobject("@function.inner", "textobjects")
-		end)
+	vim.keymap.set({ "x", "o" }, "af", function()
+		select_textobject("@function.outer", "textobjects")
+	end)
+	vim.keymap.set({ "x", "o" }, "if", function()
+		select_textobject("@function.inner", "textobjects")
 	end)
 end
 
--- lua/my/core/picker/fzf.lua
+++ lua/my/core/picker/fzf.lua
@@ -4,40 +4,37 @@ mod.setup = function()
 		"https://github.com/ibhagwan/fzf-lua",
 	})
 
-	vim.schedule(function()
-		local fzf = require("fzf-lua")
-		local fzf_config = require("fzf-lua.config")
+	local fzf = require("fzf-lua")
+	local fzf_config = require("fzf-lua.config")
 
-		fzf.setup({
-			file_icon_padding = " ",
-			fzf_opts = {
-				["--tabstop"] = 4,
+	fzf.setup({
+		file_icon_padding = " ",
+		fzf_opts = {
+			["--tabstop"] = 4,
+		},
+		winopts = {
+			preview = {
+				horizontal = "right:45%",
 			},
-			winopts = {
-				preview = {
-					horizontal = "right:45%",
-				},
-			},
+		},
+	})
+
+	fzf.nvim_config = function(opts)
+		opts = opts or {}
+		fzf.files({
+			cwd = vim.fs.dirname(vim.env.MYVIMRC),
+			fd_opts = fzf_config.defaults.files.fd_opts .. [[ --glob '*.lua' ]],
 		})
+	end
 
-		fzf.nvim_config = function(opts)
-			opts = opts or {}
-			fzf.files({
-				cwd = vim.fs.dirname(vim.env.MYVIMRC),
-				fd_opts = fzf_config.defaults.files.fd_opts
-					.. [[ --glob '*.lua' ]],
-			})
-		end
-
-		fzf.nvim_packs = function(opts)
-			opts = opts or {}
-			fzf.files({
-				cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "site", "pack"),
-				fd_opts = fzf_config.defaults.files.fd_opts
-					.. [[ --full-path --glob '**/{plugin,lua}/**/*.lua' ]],
-			})
-		end
-	end)
+	fzf.nvim_packs = function(opts)
+		opts = opts or {}
+		fzf.files({
+			cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "site", "pack"),
+			fd_opts = fzf_config.defaults.files.fd_opts
+				.. [[ --full-path --glob '**/{plugin,lua}/**/*.lua' ]],
+		})
+	end
 end
 
 return mod
