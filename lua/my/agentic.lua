5d57b2fddf2cca236746faa1eb2754acd55e3891
Isaac <minimaleffort769@gmail.com>
2026-07-02 15:06:23 +0700

    feat(agentic): update sizing and position

--- lua/my/agentic.lua
+++ lua/my/agentic.lua
@@ -1,22 +1,22 @@
 local mod = {}
 mod.setup = function()
 	local agentic = require("toggleterm.terminal").Terminal:new({
-		display_name = "Agent",
+		display_name = "AGENT",
 		cmd = "opencode",
 		hidden = true,
 		direction = "float",
 		close_on_exit = true,
 		float_opts = {
 			title_pos = "center",
-			row = 0,
+			row = 2,
 			col = function()
 				return vim.o.columns
 			end,
 			width = function()
-				return math.floor(vim.o.columns * 0.33)
+				return math.floor(vim.o.columns * 0.35)
 			end,
 			height = function()
-				return math.floor(vim.o.lines * 0.95)
+				return math.floor(vim.o.lines * 0.9)
 			end,
 		},
 	})
