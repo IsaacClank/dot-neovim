eec83514063fa483b39014734691d9ada1d13aec
Isaac <minimaleffort769@gmail.com>
2026-07-20 12:33:23 +0700

    feat(editing): add nvim-treesitter

--- lua/my/core/editing.lua
+++ lua/my/core/editing.lua
@@ -8,6 +8,7 @@ function mod.setup()
 		"https://github.com/nvim-mini/mini.surround",
 		"https://github.com/nvim-mini/mini.splitjoin",
 
+		"https://github.com/nvim-treesitter/nvim-treesitter",
 		"https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
 	})
 
@@ -17,6 +18,16 @@ function mod.setup()
 	require("mini.pairs").setup({})
 	require("mini.surround").setup({})
 	require("mini.splitjoin").setup({})
+
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = { "lua", "typescript" },
+		callback = function()
+			vim.treesitter.start()
+			vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+			vim.wo[0][0].foldmethod = "expr"
+		end,
+	})
+
 	require("nvim-treesitter-textobjects").setup({})
 
 	local select_textobject =
